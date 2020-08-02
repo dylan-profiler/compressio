020d54e9302551790b70f5e90892e700ec54fb0f
Ian Eaves <ian.k.eaves@gmail.com>
2020-08-02 14:54:58 -0500

    black ;

--- src/compressario/formatter.py
+++ src/compressario/formatter.py
@@ -9,19 +9,31 @@ class StorageSize:
     type_options = {"b": "byte", "kb": "kilobyte", "mb": "megabyte", "gb": "gigabyte"}
 
     def __init__(self, total: Union[int, float], units="mb") -> None:
-        self.total: Quantity = total * ureg.byte
-        self.units: str = units
+        self._unit_str = self.type_options[units.lower()]
+        self.total: Quantity = total * self._pint_unit
 
     @property
-    def desired_unit(self):
-        return getattr(ureg, self.type_options[self.units.lower()])
+    def units(self) -> str:
+        return self._unit_str
+
+    @units.setter
+    def units(self, val: str) -> None:
+        trial_val = val.lower()
+        if trial_val in self.type_options:
+            self._unit_str = self.type_options[trial_val]
+            self.total = self.convert_to_units(self.total)
+        else:
+            raise ValueError(f"Units must be one of {list(self.type_options.keys())}")
 
     @property
-    def _in_units(self) -> float:
-        return self.total.to(self.desired_unit)
+    def _pint_unit(self) -> Quantity:
+        return getattr(ureg, self.units)
+
+    def convert_to_units(self, value) -> float:
+        return value.to(self.units)
 
     def __str__(self) -> str:
-        return f"{self._in_units}"
+        return f"{self.total}"
 
     def __repr__(self) -> str:
         return self.__str__()
